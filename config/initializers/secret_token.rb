3c1521af2b61dd5b5ed9689c1316ceff13d65a07
Lindy Sinclair <lindyrp@gmail.com>
2016-04-23 15:23:36 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookslist::Application.config.secret_key_base = '01b445113c11f8da57e7e7c825f7c44dc78dc758838dec35eb3d5f53223486507dede481abb271a43a5d3459fbb4843cadeb11ec606ef7a010198f9e4ab13525'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bookslist::Application.config.session_store :cookie_store, key: '_bookslist_session'
